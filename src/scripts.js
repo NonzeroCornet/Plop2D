1da9d5d03f0f038a37b6e42c3706b94deda254d0
NonzeroCornet34 <ullerbri000@gmail.com>
2023-09-02 20:35:54 -0600

    yup

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,15 @@
+function generateObjectId() {
+  return Math.random().toString(36).substr(2, 9);
+}
+var hierarchyData;
 document.addEventListener("DOMContentLoaded", function () {
-  const main = document.querySelector(".main");
-  const sections = document.querySelectorAll(".section");
+  hierarchyData = {};
+  let selectedObject = null;
+
+  var main = document.querySelector(".main");
+  var sections = document.querySelectorAll(".section");
+  var gameHierarchy = document.getElementById("gameHierarchy");
+  var hierarchyMenu = document.getElementById("contextMenu");
 
   // Create resizable panels using Split.js
   Split(sections, {
@@ -17,19 +26,113 @@ document.addEventListener("DOMContentLoaded", function () {
     snapOffset: 0,
   });
 
-  const playButton = document.getElementById("playButton");
-  const codeButton = document.getElementById("codeButton");
-  const settingsButton = document.getElementById("settingsButton");
+  var playButton = document.getElementById("playButton");
+  var settingsButton = document.getElementById("settingsButton");
 
   playButton.addEventListener("click", function () {
     console.log("Play button clicked");
   });
 
-  codeButton.addEventListener("click", function () {
-    console.log("Code button clicked");
-  });
-
   settingsButton.addEventListener("click", function () {
     console.log("Settings button clicked");
   });
+
+  gameHierarchy.addEventListener("contextmenu", function (event) {
+    event.preventDefault(); // Prevent default context menu from appearing
+    var x = event.clientX;
+    var y = event.clientY;
+    hierarchyMenu.style.display = "block";
+    hierarchyMenu.style.left = x + "px";
+    hierarchyMenu.style.top = y + "px";
+  });
+
+  document.addEventListener("click", function (event) {
+    if (!hierarchyMenu.contains(event.target)) {
+      hierarchyMenu.style.display = "none";
+    }
+  });
+
+  function refreshHierarchyView() {
+    gameHierarchy.innerHTML = "";
+    createHierarchyView(null, gameHierarchy);
+  }
+
+  function createHierarchyView(parentId, container) {
+    for (var objectId in hierarchyData) {
+      if (!hierarchyData[objectId].parent && parentId === null) {
+        var hierarchyItem = document.createElement("div");
+        hierarchyItem.className = "hierarchy-item";
+        hierarchyItem.textContent = `Object ID: ${hierarchyData[objectId].id}`;
+        hierarchyItem.addEventListener("contextmenu", function (event) {
+          event.preventDefault();
+          showContextMenu(objectId, event.clientX, event.clientY);
+        });
+
+        container.appendChild(hierarchyItem);
+
+        if (hierarchyData[objectId].children.length > 0) {
+          var childContainer = document.createElement("div");
+          childContainer.className = "child-container";
+          hierarchyItem.appendChild(childContainer);
+
+          createHierarchyView(objectId, childContainer);
+        }
+      } else if (
+        hierarchyData[objectId].parent &&
+        hierarchyData[objectId].parent.id === parentId
+      ) {
+        // Similar structure as above with context menu added
+      }
+    }
+  }
+
+  function showContextMenu(objectId, x, y) {
+    var contextMenu = document.getElementById("contextMenu");
+    contextMenu.style.display = "block";
+    contextMenu.style.left = x + "px";
+    contextMenu.style.top = y + "px";
+
+    var addChildItem = document.getElementById("addChild");
+    var moveUpItem = document.getElementById("moveUp");
+    var moveDownItem = document.getElementById("moveDown");
+    var nestItem = document.getElementById("nest");
+    var removeItem = document.getElementById("remove");
+
+    addChildItem.addEventListener("click", function () {
+      console.log("pop");
+      hierarchyData[objectId].children.push(generateObjectId());
+      refreshHierarchyView();
+      contextMenu.style.display = "none";
+    });
+
+    moveUpItem.addEventListener("click", function () {
+      moveObjectUp(objectId);
+      refreshHierarchyView();
+      contextMenu.style.display = "none";
+    });
+
+    moveDownItem.addEventListener("click", function () {
+      moveObjectDown(objectId);
+      refreshHierarchyView();
+      contextMenu.style.display = "none";
+    });
+
+    nestItem.addEventListener("click", function () {
+      nestObject(objectId);
+      refreshHierarchyView();
+      contextMenu.style.display = "none";
+    });
+
+    removeItem.addEventListener("click", function () {
+      removeObject(objectId);
+      refreshHierarchyView();
+      contextMenu.style.display = "none";
+    });
+
+    document.addEventListener("click", function (event) {
+      if (!contextMenu.contains(event.target)) {
+        contextMenu.style.display = "none";
+      }
+    });
+  }
 });
